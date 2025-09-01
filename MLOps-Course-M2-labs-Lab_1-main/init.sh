02cc0333d8f835611178defbdbe69794b6def891
DnB <Banzouzidilou@gmail.com>
2025-09-01 16:02:05 +0200

    Delete MLOps-Course-M2-labs-Lab_1-main directory

--- MLOps-Course-M2-labs-Lab_1-main/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-@echo "Creating Python virtual environment 'MLOps-env"; \
-python3 -m venv MLOps-env; \
-@echo "Activating Python virtual environment 'MLOps-env"; \
-source MLOps-env/bin/activate; 
-@echo "Installing requirements"; \
-pip3 install -r requirements.txt; \
-echo "Initialization complete"
